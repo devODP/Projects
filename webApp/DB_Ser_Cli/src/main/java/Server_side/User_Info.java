bdec5c96f206c0784131524ae893b7617c474c9d
devODP <devODP@users.noreply.github.com>
2017-11-14 01:54:00 -0500

    Add files via upload
    Updated coding styles according to OCPJP contents

--- webApp/DB_Ser_Cli/src/main/java/Server_side/User_Info.java
+++ webApp/DB_Ser_Cli/src/main/java/Server_side/User_Info.java
@@ -1,21 +1,34 @@
 package Server_side;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User_Info {
 	private static boolean lock = true;
+	private static String messageToUser = "";
 	private static String address = "";
 	private static int portNumber = -1;
 	private static String fileToUpload[] = new String[2];
 	private static boolean isEmptyFile = true;
 
-	User_Info(String addr, int port) {
+	public User_Info(String addr, int port) {
 		if (address.equals("")) {
 			address = addr;
 			portNumber = port;
 			lock = false;
+			messageToUser = "";
 		}
 	}
 	
 	public User_Info(){
+	}
+	
+	public void setMessage(String str){
+		messageToUser = str;
+	}
+	
+	public String getMessage(){
+		return messageToUser;
 	}
 	
 	public boolean isFileEmpty(){
