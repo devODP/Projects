48bfca1e64d8b33fb7538fd29393db7751e91966
devODP <devODP@users.noreply.github.com>
2017-11-27 18:26:27 -0500

    Add files via upload
    Keep track of a list of active clients information

---
+++ webApp/Verification from JEE Materials/EJBs/Attendance.java
@@ -0,0 +1,35 @@
+package core;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Startup
+@Singleton
+public class Attendance {
+	private Map<String, ArrayList<Boolean>> attendanceList = new HashMap<>();
+	
+	public Attendance(){}
+	
+	public void putIPStatus(String ip, ArrayList<Boolean> statList){
+		attendanceList.put(ip, statList);
+	}
+	
+	public ArrayList<Boolean> getIPStatus(String ip){
+		return new ArrayList<Boolean>(attendanceList.get(ip));
+	}
+	
+	public void removeIP(String ip){
+		attendanceList.remove(ip);
+	}
+
+	public boolean containsIP(String ip){
+		return attendanceList.containsKey(ip);
+	}
+	
+	public void updateStatus(String ip, ArrayList<Boolean> statList){
+		attendanceList.replace(ip, statList);
+	}
+}
