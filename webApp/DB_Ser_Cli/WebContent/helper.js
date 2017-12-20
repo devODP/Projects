3becfa3744087478485ce264e376b25d591361f3
devODP <devODP@users.noreply.github.com>
2017-12-20 02:57:37 -0500

    added webRTC mechanism

--- webApp/DB_Ser_Cli/WebContent/helper.js
+++ webApp/DB_Ser_Cli/WebContent/helper.js
@@ -14,7 +14,9 @@ ws.onmessage = function(event) {
 };
 		
 ws.onopen = function(){
-	ws.send(document.location.host.substr(0, document.location.host.indexOf(":")));
+	getUserIP(function(ip){
+		ws.send(ip);
+	});
 };
 
 function sendMessage() {
@@ -31,4 +33,42 @@ function connectUpload(){
 		
 function connectInsert(){
 	document.insert.action = "http://" + document.location.host + "/DB_Ser_Cli/insert";
+}
+
+function getUserIP(privateIP) {
+    //compatibility for firefox, chrome and IE Edge
+    var PeerConnection = window.mozRTCPeerConnection || window.RTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new PeerConnection({
+        iceServers: []
+    }), noop = function() {}, localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+    function iterateIP(ip) {
+        if (!localIPs[ip]){
+        	privateIP(ip);
+        }
+        localIPs[ip] = true;
+    }
+    //create a data channel for IP address transmition
+    pc.createDataChannel("Private IP address");
+    // create offer and set local description
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0){
+            	return;
+            }
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp);
+    }).catch(function(e) {
+        console.error(e);
+    });
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)){
+        	return;
+        }
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
 }
